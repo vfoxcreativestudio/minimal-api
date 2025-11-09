5caa42b25ef7aa519ec5aed2d5e7f73a7f5a3ae1
snowfox_x <vfoxcreativestudio@gmail.com>
2025-11-09 00:08:41 -0300

    Iniciando o Projeto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System.CodeDom.Compiler;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login com Sucesso");
+    else
+        return Results.Unauthorized();
+
+});
+
+
+app.Run();
+
+
